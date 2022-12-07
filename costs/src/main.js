99379a36279969e6294c67382c583d4ea8f7a09b
Kate <belporibet@mail.ru>
2022-12-07 22:02:26 +0300

    lesson7

--- costs/src/main.js
+++ costs/src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ModalPlugin from './plagins/ModalPlugin'
+
 Vue.config.productionTip = false
+Vue.use(ModalPlugin)
 
 new Vue({
   router,
--- costs/src/views/HomeView.vue
+++ costs/src/views/HomeView.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
+    <button v-on:click="showModal">Добавить+</button>
     <List />
     <div class="pagination">
       <button v-on:click="$store.commit('prevPage')">prev</button>
       <button v-on:click="$store.commit('nextPage')">next</button>
     </div>
+    <AddModal />
+
   </div>
 </template>
 
 <script>
 import List from '@/components/List.vue';
-
+import AddModal from '@/components/AddModal.vue';
 export default {
   name: 'HomeView',
-  components: { List }
+  components: { List, AddModal },
+  methods: {
+    showModal() {
+      this.$modal.show('add')
+    }
+  }
 }
 </script>
